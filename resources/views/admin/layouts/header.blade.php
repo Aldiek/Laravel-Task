f4b725467669dd7004102f386aca5a7eaeb16be0
aldiek <aldiek@gmail.com>
2020-12-07 06:05:59 +0200

    added css

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -8,25 +8,26 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/fontawesome-free/css/all.min.css" />
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/plugins/fontawesome-free/css/all.min.css" />
     <!-- Ionicons -->
     <link rel="stylesheet"
-        href="{{ url('/') }}/adminLTE/https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+        href="{{ secure_asset('/') }}/adminLTE/https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
     <!-- Tempusdominus Bbootstrap 4 -->
     <link rel="stylesheet"
-        href="{{ url('/') }}/adminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css" />
+        href="{{ secure_asset('/') }}/adminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css" />
     <!-- iCheck -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css" />
     <!-- JQVMap -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/jqvmap/jqvmap.min.css" />
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/plugins/jqvmap/jqvmap.min.css" />
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/dist/css/adminlte.min.css" />
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/dist/css/adminlte.min.css" />
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css" />
+    <link rel="stylesheet"
+        href="{{ secure_asset('/') }}/adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css" />
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/daterangepicker/daterangepicker.css" />
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/plugins/daterangepicker/daterangepicker.css" />
     <!-- summernote -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/summernote/summernote-bs4.css" />
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/plugins/summernote/summernote-bs4.css" />
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet" />
     <link href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" type="text/css" rel="stylesheet" />
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,13 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ url('/') }}/adminLTE/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{ secure_asset('/') }}/adminLTE/dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -75,11 +75,11 @@
     <!-- /.login-box -->
 
     <!-- jQuery -->
-    <script src="{{ url('/') }}/adminLTE/plugins/jquery/jquery.min.js"></script>
+    <script src="{{ secure_asset('/') }}/adminLTE/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ url('/') }}/adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ secure_asset('/') }}/adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="{{ url('/') }}/adminLTE/dist/js/adminlte.min.js"></script>
+    <script src="{{ secure_asset('/') }}/adminLTE/dist/js/adminlte.min.js"></script>
 
 </body>
 
