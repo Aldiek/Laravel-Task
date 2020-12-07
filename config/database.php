5717134427a457ca67e19521da3a6292db186b4d
aldiek <aldiek@gmail.com>
2020-12-07 07:44:35 +0200

    added sql

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'sql2380672'),
             'username' => env('DB_USERNAME', 'sql2380672'),
-            'password' => env('DB_PASSWORD', 'Start@4X'),
+            'password' => env('DB_PASSWORD', 'iW2*uR7*'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
